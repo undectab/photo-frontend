0bb351b259b3a31442a010a72fb6e33fff636966
undectab <tuncahmetbayram@gmail.com>
2025-08-02 02:27:10 +0300

    qwe

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
+   base: './',
   plugins: [
     vue(),
     Pages(), // 📌 burada pages plugin'i eklendi
